d9e43bd042b24420c4401740ae175142e29f41a2
mehradsadeghi <2012.linkinpark@gmail.com>
2020-07-24 23:06:39 +0430

    Making the basics of test environment based on an in-memory database.

--- tests/BasicTest.php
+++ tests/BasicTest.php
@@ -2,11 +2,22 @@
 
 namespace Mehradsadeghi\FilterQueryString\Tests;
 
+use Illuminate\Support\Facades\Route;
+use Mehradsadeghi\FilterQueryString\Models\User;
+
 class BasicTest extends TestCase
 {
     /** @test */
     public function sample()
     {
+        Route::get('/', function() {
+            return User::all();
+        });
+
+        $response = $this->get('/');
+
+        dd($response->getContent());
+
         $this->assertTrue(true);
     }
 }
